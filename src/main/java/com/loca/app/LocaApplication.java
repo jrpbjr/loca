801f2e1a9effc596f2d829ee4891f8a098110be1
jrpbjr <jrpbjr@gmail.com>
2021-07-06 17:02:35 -0300

    inicio de projeto e configuração dos properties

---
+++ src/main/java/com/loca/app/LocaApplication.java
@@ -0,0 +1,13 @@
+package com.loca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loca/app/LocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
