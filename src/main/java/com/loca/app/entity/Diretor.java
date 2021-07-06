92241b52f12faeee5eec33043f20e7cbe7da1b37
jrpbjr <jrpbjr@gmail.com>
2021-07-06 19:23:59 -0300

    entidade diretor

---
+++ src/main/java/com/loca/app/entity/Diretor.java
@@ -0,0 +1,36 @@
+package com.loca.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Diretor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50)
+	@NotBlank(message = "Nome do Diretor deve ser preenchido")
+	@Size(min = 2, max = 50, message = "Nome do Diretor deve ter entre 2 e 50 letras")
+    private String nome;
+
+}
