42c3706576c1b171df59b3294e3cdab54a6a0bc2
jrpbjr <jrpbjr@gmail.com>
2021-07-06 21:27:01 -0300

    Classe criada para representar a chave primária composta do objeto Multa

---
+++ src/main/java/com/loca/app/entity/MultaPK.java
@@ -0,0 +1,28 @@
+package com.loca.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+//Classe criada para representar a chave primária composta do objeto Multa
+@Embeddable
+@Data
+@EqualsAndHashCode(of = {"itemDeEmprestimo"})
+public class MultaPK implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @OneToOne
+  @JoinColumns({
+      @JoinColumn(name = "FITA_ID", referencedColumnName = "FITA_ID"),
+      @JoinColumn(name = "EMPRESTIMO_ID", referencedColumnName = "EMPRESTIMO_ID")
+  })
+  private ItemDeEmprestimo itemDeEmprestimo = new ItemDeEmprestimo();
+
+}
