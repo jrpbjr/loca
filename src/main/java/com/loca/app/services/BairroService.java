385ce1c26c08ef43750366658f888102d6450eb9
jrpbjr <jrpbjr@gmail.com>
2021-07-07 14:07:20 -0300

    Bairro Services

---
+++ src/main/java/com/loca/app/services/BairroService.java
@@ -0,0 +1,62 @@
+package com.loca.app.services;
+
+
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.loca.app.entity.Bairro;
+import com.loca.app.repositories.BairroRepository;
+import com.loca.app.services.exceptions.DataIntegrityException;
+import com.loca.app.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class BairroService {
+
+    @Autowired
+    private BairroRepository repository;
+
+    public Bairro findById(Integer id) {
+    	try {
+        	Bairro obj = repository.findById(id).get();
+        	return obj;
+        } catch (NoSuchElementException e) {
+        	throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Bairro.class.getName());
+        }
+    }
+
+//    public Collection<Bairro> findAll() {
+//        return repository.findAll();
+//    }
+//
+//    public Bairro insert(Bairro obj) {
+//    	obj.setId(null);
+//        try {
+//        	return repository.save(obj);
+//        } catch (DataIntegrityViolationException e) {
+//            throw new DataIntegrityException("Campo(s) obrigatório(s) do Bairro não foi(foram) preenchido(s): Cidade");
+//        }
+//    }
+//
+//    public Bairro update(Bairro obj) {
+//    	findById(obj.getId());
+//        try {
+//        	return repository.save(obj);
+//        } catch (DataIntegrityViolationException e) {
+//            throw new DataIntegrityException("Campo(s) obrigatório(s) do Bairro não foi(foram) preenchido(s): Cidade");
+//        }
+//    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir um Bairro associado a uma Pessoa (gerente, funcionário ou cliente)!");
+        }
+    }
+
+}
