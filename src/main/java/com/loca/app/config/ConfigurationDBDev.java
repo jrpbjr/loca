16bd7f16793b40d1434e1ea55fef64d2fc3422f7
jrpbjr <jrpbjr@gmail.com>
2021-07-07 22:43:50 -0300

    popular o banco de dados

--- src/main/java/com/loca/app/config/ConfigurationDBDev.java
+++ src/main/java/com/loca/app/config/ConfigurationDBDev.java
@@ -1,5 +1,37 @@
 package com.loca.app.config;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.loca.app.services._DBService;
+
+/**Classe para configuração do ambiente de Desenvolvimento
+* Neste caso, estamos armazenando no banco de dados localhost
+*/
+
+@Configuration
+@Profile("dev")
 public class ConfigurationDBDev {
+
+    @Autowired
+    private _DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instatiateDatabase() throws ParseException, IOException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
 
 }
--- src/main/java/com/loca/app/config/ConfigurationDBTest.java
+++ src/main/java/com/loca/app/config/ConfigurationDBTest.java
@@ -1,5 +1,29 @@
 package com.loca.app.config;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.loca.app.services._DBService;
+
+/**Classe para configuração do ambiente de Teste
+* Neste caso, estamos armazenando no banco de dados H2
+*/
+@Configuration
+@Profile("test")
 public class ConfigurationDBTest {
 
+	@Autowired
+	private _DBService dbService;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException, IOException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
 }
