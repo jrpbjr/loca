de9dd6b372b5530d9766f4bccfd0da7d20acd72f
jrpbjr <jrpbjr@gmail.com>
2021-07-07 22:47:13 -0300

    configuração de cors

---
+++ src/main/java/com/loca/app/config/ConfigurationCors.java
@@ -0,0 +1,30 @@
+package com.loca.app.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**Classe para configuração de Cors 
+* Neste caso, estamos habilitando as requisições PUT e DELETE
+*
+*/
+
+@Configuration
+public class ConfigurationCors {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
